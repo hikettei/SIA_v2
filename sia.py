d19392bfa78eb9a0a2ae11cc684bd4ea13c7c798
hikettei <hikettei@gmail.com>
2022-07-22 19:41:07 +0900

    refactor

--- sia.py
+++ sia.py
@@ -49,133 +49,6 @@ class IndexAttentionSort(nn.Module):
 		reference = reference.squeeze(1)
 		return (weight_map * reference).view(xs.shape[0], -1, len(reference[0][0]))
 
-# Inputs [batch_size, seq_len, d_model]
-# Outputs [batch_size, seq_len, d_model]
-
-# if you need use it as self-attention like, for example,
-# hint: set submodule == nn.Conv1d(), swap axes (1, 2), before and after of passing this module.
-class WithLSHSort(nn.Module):
-    def __init__(self,
-            d_model=512,
-            n_heads=8,
-            submodule=nn.Identity(),
-            eps=1e-4
-            ):
-        super(WithLSHSort, self).__init__()
-        assert d_model % n_heads == 0, f"d_model must be able to devided by n_heads"
-        self.hash = nn.ModuleList([nn.Linear(d_model // n_heads, 2) for _ in range(d_model // n_heads)])
-        self.n_heads = n_heads
-        self.d_head = d_model // n_heads
-        self.d_model = d_model
-        self.mod = submodule
-        self.eps = 1e-4
-
-    def forward(self, x):
-        # caluclate indexes
-
-        projected = torch.cat([self.hash[n](head) for n, head in zip(range(self.n_heads), torch.split(x, self.d_head, dim=2))], dim=2)
-        h_x, h_y = torch.split(projected, self.n_heads, dim=2) # [batch_size, seq_len, nheads] where h_x, h_y
-        angles = torch.arctan(h_x / (h_y + self.eps)) # [batch_size, seq_len, n_heads] # calculate angle of vector
-        indexes = torch.argsort(angles, 1) # [batch_size, seq_len, n_heads]
-        indexes = torch.unsqueeze(indexes, dim=3).expand(-1, -1, -1, self.d_head) # [batch_size, seq_len, n_heads, d_head]
-        
-        # split heads
-        x = x.reshape(x.shape[0], x.shape[1], self.n_heads, self.d_head)
-
-        # sort heads
-        x = torch.gather(x, 1, indexes)
-
-        # concatenate heads
-        x = x.reshape(x.shape[0], x.shape[1], self.d_model)
-                
-    
-        # call module
-        x = self.mod(x)
-        
-        # split heads
-        x = x.reshape(x.shape[0], x.shape[1], self.n_heads, self.d_head)
-
-        
-        # scatter
-        x = torch.scatter(x ,1, indexes, x)
-
-        # concatenate heads
-        x = x.reshape(x.shape[0], x.shape[1], self.d_model)
-        return x
-
-# convolution with swap axes.
-class Conv1dForLSHSort(nn.Module):
-    def __init__(self, d_model, kernel_size, stride, padding, padding_mode='circular', **kwargs):
-        super(Conv1dForLSHSort, self).__init__()
-        self.conv = nn.Conv1d(d_model, d_model, kernel_size, stride, padding, padding_mode=padding_mode, **kwargs)
-    def forward(self, x):
-        x = x.swapaxes(1,2)
-        x = self.conv(x)
-        x = x.swapaxes(1,2)
-        return x
-
-
-# convolution with swap axes.
-class Conv1dForLSHSort(nn.Module):
-    def __init__(self, d_model, kernel_size, stride, padding, padding_mode='circular', **kwargs):
-        super(Conv1dForLSHSort, self).__init__()
-        self.conv = nn.Conv1d(d_model, d_model, kernel_size, stride, padding, padding_mode=padding_mode, **kwargs)
-    def forward(self, x):
-        x = x.swapaxes(1,2)
-        x = self.conv(x)
-        x = x.swapaxes(1,2)
-        return x
-
-class MultiheadAttentionForLSHSort(nn.Module):
-    def __init__(self, d_model, segment_size=4, n_heads=8, logger=nn.Identity()):
-        super(MultiheadAttentionForLSHSort, self).__init__()
-        self.attn = nn.MultiheadAttention(d_model, n_heads, batch_first=True)
-        self.seg_size = segment_size
-        self.proj_qk = nn.Linear(d_model, d_model)
-        self.proj_v = nn.Linear(d_model, d_model)
-        self.logger=logger
-    def forward(self, x):
-        # x shape = [batch_size, seq_len, d_model]
-        # convert to [batch_size*seg_size, seq_len//seg_size, d_model]
-
-        # pad
-        pad_seq_len = self.seg_size - (x.shape[1] % self.seg_size)
-        seq_len = x.shape[1]
-        a = (seq_len + pad_seq_len) // self.seg_size
-        x = torch.cat([x, x[:, 0:pad_seq_len, :]], dim=1)
-        x = torch.cat(torch.chunk(x, a, dim=1), dim=0) # pack to batch
-        self.logger(f"Splitted attention {a} blocks, {self.seg_size} tokens per block")
-        mask = torch.diag(torch.BoolTensor(self.seg_size)).to(x.device)
-        x, _ = self.attn(self.proj_qk(x), self.proj_qk(x), self.proj_v(x), attn_mask=mask)
-        x = torch.cat(torch.chunk(x, a, dim=0), dim=1)
-        x = x[:, 0:seq_len, :]
-        return x
-
-
-class LSHAttention(nn.Module):
-    def __init__(self, d_model, n_heads=8, segment_size=4, logger=nn.Identity()):
-        super(LSHAttention, self).__init__()
-        self.seq = WithLSHSort(d_model, n_heads, MultiheadAttentionForLSHSort(d_model, segment_size=segment_size, n_heads=n_heads, logger=logger))
-
-    def forward(self, x):
-        return self.seq(x)
-
-
-class LSHConv(nn.Module):
-    def __init__(self, d_model, n_heads, kernel_size=3, stride=1, padding=1, padding_mode='circular', groups=None, bias=True):
-
-        super(LSHConv, self).__init__()
-
-        if not groups:
-            groups = n_heads
-        submodule = Conv1dForLSHSort(d_model, kernel_size, stride, padding, padding_mode, groups=groups, bias=bias)
-        self.lsh_module = WithLSHSort(d_model, n_heads, submodule)
-
-    def forward(self,x):
-        return self.lsh_module(x)
-
-    def forward(self,x):
-        return self.lsh_module(x)
 
 class FFN(nn.Module):
     def __init__(self, d_model: int, d_ff: int) -> None:
@@ -187,22 +60,17 @@ class FFN(nn.Module):
         return self.linear2(F.relu(self.linear1(x)))
 
 class SIAEncoder(nn.Module):
-	def __init__(self, vocab_size, d_model, pad_idx, dropout, maxlen, hidden_size=256, d_ff=256, layer_norm_eps=1e-3, encoder_layer_num=6, name="light", n_heads=8, device=torch.device("cpu")):
+	def __init__(self, vocab_size, d_model, pad_idx, dropout, maxlen, hidden_size=256, d_ff=256, layer_norm_eps=1e-3, encoder_layer_num=6, n_heads=8, device=torch.device("cpu")):
 		super().__init__()
 		self.embedding = SentenceEmbedding(vocab_size, d_model, pad_idx, dropout, maxlen)
 		self.iattention = IndexAttentionSort(d_model)
 		self.device = device
 		self.pad_idx = pad_idx
 		self.train_source = True
-		self.name = name
 		self.hidden_size = hidden_size
-		def new_model(name):
-			if name == "light":
-				return LightEncoder(d_model, hidden_size, d_ff=d_ff, dropout=dropout, layer_norm_eps=layer_norm_eps)
-			else:
-				return EncoderLayer(d_model, d_ff, dropout, layer_norm_eps, n_heads=n_heads)
+
 		self.encoder_layers = nn.ModuleList([
-			new_model(name)
+			LightEncoder(d_model, hidden_size, d_ff=d_ff, dropout=dropout, layer_norm_eps=layer_norm_eps)
 			for _ in range(encoder_layer_num)])
 
 	def forward(self, xs, ys, reference):
@@ -212,9 +80,6 @@ class SIAEncoder(nn.Module):
 		# ys ... [[1,2,3], [4,5,6] ...]
 		reference_embedding = self.make_embedding(reference)
 
-		def make_mask(tensor, l):
-			pass
-
 		x = self.make_embedding(xs)
 		y = self.make_embedding(ys)
 
@@ -223,16 +88,12 @@ class SIAEncoder(nn.Module):
 		# context informations
 		
 		x = torch.concat([reference_embedding, x], dim=1)
-		#y = torch.concat([y, reference_embedding], dim=1)
+
 
 		hidden = torch.zeros(1, x.shape[0], self.hidden_size)
 
-		if self.name == "light":
-			for encoder_layer in self.encoder_layers:
-				x, hidden = encoder_layer(x, hidden)
-		else:
-			for encoder_layer in self.encoder_layers:
-				x = encoder_layer(x)
+		for encoder_layer in self.encoder_layers:
+			x, hidden = encoder_layer(x, hidden)
 		return x, y, hidden
 
 
@@ -250,81 +111,18 @@ class SIAEncoder(nn.Module):
 		return L
 
 class SIADecoder(nn.Module):
-	def __init__(self, d_model, dropout, d_ff=256, layer_norm_eps=1e-3, hidden_size=256, decoder_layer_num=6, n_heads=8, name="light"):
+	def __init__(self, d_model, dropout, d_ff=256, layer_norm_eps=1e-3, hidden_size=256, decoder_layer_num=6, n_heads=8):
 		super().__init__()
-
-		def new_model(name):
-			if name == "light":
-				return LightDecoder(d_model, hidden_size, d_ff=d_ff, dropout=dropout, layer_norm_eps=layer_norm_eps)
-			else:
-				return DecoderLayer(d_model, d_ff, dropout, layer_norm_eps, n_heads=n_heads)
 
 		self.hidden_size = hidden_size
-		self.name = name
 		self.decoder_layers = nn.ModuleList([
-			new_model(name)
+			LightDecoder(d_model, hidden_size, d_ff=d_ff, dropout=dropout, layer_norm_eps=layer_norm_eps)
 			for _ in range(decoder_layer_num)])
+
 	def forward(self, x, y, hidden):
-		if self.name == "light":
-			for decoder_layer in self.decoder_layers:
-				x, y, hidden = decoder_layer(x, y, hidden)
-		else:
-			for decoder_layer in self.decoder_layers:
-				y = decoder_layer(x, y)
+		for decoder_layer in self.decoder_layers:
+			x, y, hidden = decoder_layer(x, y, hidden)
 		return y
-
-class EncoderLayer(nn.Module):
-	def __init__(self, d_model, d_ff, dropout_rate, layer_norm_eps, n_heads=8):
-		super().__init__()
-
-		self.attention = LSHAttention(d_model, n_heads=n_heads)
-		self.dropout_attention = nn.Dropout(dropout_rate)
-		self.layer_norm_attention = nn.LayerNorm(d_model, eps=layer_norm_eps)
-
-		self.ffn = FFN(d_model, d_ff)
-		self.dropout_ffn = nn.Dropout(dropout_rate)
-		self.layer_norm_ffn = nn.LayerNorm(d_model, eps=layer_norm_eps)
-
-	def forward(self, x):
-		x = self.layer_norm_attention(
-			self.dropout_attention(
-					self.attention(x)))
-
-		x = self.layer_norm_ffn(
-			self.dropout_ffn(self.ffn(x)))
-
-		return x
-
-class DecoderLayer(nn.Module):
-	def __init__(self, d_model, d_ff, dropout_rate, layer_norm_eps, n_heads=8):
-		super().__init__()
-
-		self.src_attention = Ref_LSHAttention(n_hashes=n_heads)#nn.MultiheadAttention(embed_dim=d_model, num_heads=n_heads)
-		self.tgt_attention = LSHAttention(d_model, n_heads=n_heads)
-		self.ffn           = FFN(d_model, d_ff)
-
-		self.dropout_src_attention = nn.Dropout(dropout_rate)
-		self.dropout_tgt_attention = nn.Dropout(dropout_rate)
-		self.dropout_ffn           = nn.Dropout(dropout_rate)
-
-		self.layer_norm_src_attention = nn.LayerNorm(d_model, eps=layer_norm_eps)
-		self.layer_norm_tgt_attention = nn.LayerNorm(d_model, eps=layer_norm_eps)
-		self.layer_norm_ffn           = nn.LayerNorm(d_model, eps=layer_norm_eps)
-
-	def forward(self, x, y):
-		y = self.layer_norm_src_attention(
-			y + self.dropout_src_attention(
-				self.src_attention(y, x)[0])) #q, k, v=y,x,x | qk, v = x, y
-
-		x = self.layer_norm_tgt_attention(
-			y + self.dropout_tgt_attention(
-					self.tgt_attention(x)))
-
-		x = self.layer_norm_ffn(
-			x + self.dropout_ffn(
-				self.ffn(x)))
-
-		return x
 
 class LightEncoder(nn.Module):
 	def __init__(self, d_model, hidden_size, d_ff=512, dropout=0.01, layer_norm_eps=1e-3):
@@ -375,7 +173,6 @@ class SIA(nn.Module):
 		encoder_layer_num=6,
 		decoder_layer_num=6,
 		n_heads=1,
-		name="light",
 		device=torch.device("cpu")):
 
 		super().__init__()
@@ -385,10 +182,9 @@ class SIA(nn.Module):
 			device=device,
 			n_heads=n_heads,
 			hidden_size=d_model,
-			encoder_layer_num=encoder_layer_num,
-			name=name)
+			encoder_layer_num=encoder_layer_num)
 
-		self.decoder = SIADecoder(d_model, dropout, d_ff=d_ff, hidden_size=d_model, layer_norm_eps=layer_norm_eps, decoder_layer_num=decoder_layer_num, n_heads=n_heads, name=name)
+		self.decoder = SIADecoder(d_model, dropout, d_ff=d_ff, hidden_size=d_model, layer_norm_eps=layer_norm_eps, decoder_layer_num=decoder_layer_num, n_heads=n_heads)
 		self.linear  = nn.Linear(d_model, vocab_size)
 	def forward(self, x, y, reference):
 		x_out, ys, hidden = self.encoder(x, y, reference)
